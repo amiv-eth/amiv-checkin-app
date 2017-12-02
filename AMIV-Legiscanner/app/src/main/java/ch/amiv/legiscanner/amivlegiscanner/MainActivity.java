c82e9e1084523501b2a0a23750b01e10b9a046e6
Roger Barton <roger-barton@live.co.uk>
2017-12-02 19:27:11 +0100

    More UI functionality between activities, added broken get request

--- AMIV-Legiscanner/app/src/main/java/ch/amiv/legiscanner/amivlegiscanner/MainActivity.java
+++ AMIV-Legiscanner/app/src/main/java/ch/amiv/legiscanner/amivlegiscanner/MainActivity.java
@@ -1,52 +1,84 @@
 package ch.amiv.legiscanner.amivlegiscanner;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.android.volley.*;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
-import com.google.android.gms.vision.barcode.Barcode;
-import com.google.android.gms.vision.barcode.BarcodeDetector;
-import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.os.Bundle;
 import android.util.Log;
-import android.util.SparseArray;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-import com.google.android.gms.vision.Frame;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText pinField;
+    public static String url = "http://www.google.com";
+    EditText mPinField;
+    int mCurrentPin;
+    boolean mWaitingOnServer = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        pinField = (EditText)findViewById(R.id.PinField);
+        mPinField = (EditText)findViewById(R.id.PinField);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();  // Always call the superclass method first
+
+        mWaitingOnServer = false;
+        mPinField.setText("");  //clear pin field
     }
 
     public void pinSubmit(View view)
     {
-        int pin = Integer.parseInt(pinField.getText().toString());
-        StartScanActivity(pin);
-        Log.e("pin", "pinSubmit: " + pin);
+        if(mWaitingOnServer || "".equals(mPinField.getText().toString()))  //prevents submitting a second pin while still waiting on the response for the first pin
+            return;
+        mWaitingOnServer = true;
+
+        mCurrentPin = Integer.parseInt(mPinField.getText().toString());
+/*
+        Log.e("pin", "event pin submitted to server: " + mCurrentPin);
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url + "?pin=" + mCurrentPin,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Log.e("response", "Response from server: " + response.substring(0,500) + " on event pin: " + mCurrentPin);
+
+                        if(response == "200"){
+                            StartScanActivity();
+                            mWaitingOnServer = false;
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    mWaitingOnServer = false;
+                    Log.e("response", "Server sent back error: " + error);
+                }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+        */
+
+        StartScanActivity();
     }
 
-    private void StartScanActivity(int pin)
+    private void StartScanActivity()
     {
         Intent intent = new Intent(this, ScanActivity.class);
-        intent.putExtra(ScanActivity.PIN, pin);
+        intent.putExtra(ScanActivity.PIN, mCurrentPin);
+        intent.putExtra(ScanActivity.url, url);
         startActivity(intent);
-
     }
 }
--- AMIV-Legiscanner/app/src/main/java/ch/amiv/legiscanner/amivlegiscanner/ScanActivity.java
+++ AMIV-Legiscanner/app/src/main/java/ch/amiv/legiscanner/amivlegiscanner/ScanActivity.java
@@ -2,14 +2,69 @@ package ch.amiv.legiscanner.amivlegiscanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class ScanActivity extends AppCompatActivity {
+    public static String url = "";
     public  static final String PIN = "000000";
+    boolean mWaitingOnServer = false;
+    Switch mCheckInSwitch;
+    boolean mIsCheckingIn = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+
+        mCheckInSwitch = (Switch)findViewById(R.id.CheckInSwitch);
+        mCheckInSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mIsCheckingIn = isChecked;
+            }
+        });
+    }
+
+
+    public void SubmitLegiNr(String leginr)
+    {
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url + "?pin=" + PIN + "&legi=" + leginr,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Log.e("response", "Response from server: " + response.substring(0,500) + " on event pin: " + PIN);
+
+                        if(response == "200")
+                        {
+                            Log.e("response", "Legi nr valid");
+                        }
+                        else
+                        {
+                            Log.e("response", "Legi nr NOT valid");
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        mWaitingOnServer = false;
+                        Log.e("response", "Server sent back error: " + error);
+                    }
+                });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
 }
